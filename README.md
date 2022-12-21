3609b5a0f117a836d830ba7fccbc84ca9b0d82fd
= <=>
2022-12-21 22:54:35 +0900

    project uploaded

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# 간단한 협업
+# Pull Request 프로세스
 1. repository fork
 2. fork한 repository를 local repository로 clone
 3. local repository에서 브랜치를 새로 만들어서 개인 작업
 4. 작업이 완료되었으면 본인이 작업한 브랜치를 fork repository로 push
 5. pull request 날리기
+6. 메인 repository에서 merge
 
 ### 그림설명
 ![pic](/pull_request.png)
+
+# 프로젝트 설명
---
+++ app/src/main/java/com/example/android/android_teamwork/ui/StartFragment.kt
@@ -0,0 +1,25 @@
+package com.example.android.android_teamwork.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.android.android_teamwork.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+    private lateinit var binding: FragmentStartBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
