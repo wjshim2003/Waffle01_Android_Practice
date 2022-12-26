3443b5fc698ba5b85d1af5cbec7d5437e5fa5dbf
eastshine2741 <ehdduq8635@naver.com>
2022-12-26 20:20:55 +0900

    third

--- app/src/main/java/com/example/android/android_teamwork/ui/StartFragment.kt
+++ app/src/main/java/com/example/android/android_teamwork/ui/StartFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.android.android_teamwork.R
 import com.example.android.android_teamwork.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
@@ -16,10 +18,17 @@ class StartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentStartBinding.inflate(inflater, container, false)
+        binding.button4.setOnClickListener {
+            goToEastshine()
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun goToEastshine() {
+        findNavController().navigate(R.id.action_startFragment_to_eastshineFragment)
     }
 }
